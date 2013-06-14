948a5fdc5563ffc85e25ba83052b1a24975f998a
Asura <Asura@Asura-PC>
2013-06-14 18:54:55 -0400

    final

--- ChatExample/src/ChatMessage.java
+++ ChatExample/src/ChatMessage.java
@@ -2,7 +2,6 @@
 import java.io.Serializable;
 
 public class ChatMessage implements Serializable{
-	private int number=0;
 	private String message;
 	public ChatMessage(String message){
 		this.message=message;

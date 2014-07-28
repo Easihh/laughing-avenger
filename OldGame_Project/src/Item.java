ffdb0ddaeb83ac5493ac180c116187bdb799d1d8
Easihh <EnricoTalbot@gmail.com>
2014-07-28 18:14:05 -0400

    deleting very old project

--- OldGame_Project/src/Item.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Graphics;
-import java.awt.geom.Rectangle2D;
-
-public abstract class Item {
-	abstract void draw(Graphics g);
-	abstract Rectangle2D getMask();
-	abstract boolean getOwnership();
-	abstract void acquired();
-}

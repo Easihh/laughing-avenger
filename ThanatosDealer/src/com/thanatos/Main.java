bb58bccd33931c0060acc8d9326e35f76c98eef3
Enrico Talbot <EnricoTalbot@gmail.com>
2016-06-14 21:42:23 -0400

    -Removal of dynamic sessions, no longer needed as the service server has the direct and only fix connection.

--- ThanatosDealer/src/com/thanatos/Main.java
+++ ThanatosDealer/src/com/thanatos/Main.java
@@ -13,16 +13,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import quickfix.Acceptor;
 import quickfix.DefaultMessageFactory;
-import quickfix.Dictionary;
 import quickfix.FileStoreFactory;
 import quickfix.ScreenLogFactory;
-import quickfix.SessionID;
 import quickfix.SessionSettings;
 import quickfix.SocketAcceptor;
 
 public class Main{
+	
 	private Scheduler yahooScheduler;
 	public static ApplicationContext ctx;
+	private Acceptor acceptor;
 	
 	public static void main(String[] args) {
 		new Main().start();
@@ -32,24 +32,11 @@ public class Main{
 		try {
 			setupJobs();
 			ctx=new ClassPathXmlApplicationContext("Spring.xml");
-			SessionSettings settings=new SessionSettings();
-			Dictionary d=new Dictionary();
-			d.setString("SocketReuseAddress", "Y");
-            d.setString("ConnectionType", "acceptor");
-            d.setString("SocketAcceptPort", "5001");
-            d.setString("FileLogPath", "c:\\fixlog");
-            d.setString("StartTime", "00:00:00");
-            d.setString("EndTime", "00:00:00");
-            d.setString("FileStorePath", "c:\\fixfiles");
-            d.setString("UseDataDictionary", "Y");
-            d.setString("DataDictionary", "FIX44.xml");
-			d.setString("BeginString", "FIX.4.4");
-			SessionID session=new SessionID("FIX.4.4","FixServer","CLIENT1");
-	        settings.set(session,d);
-			FixServer server=new FixServer();
+			SessionSettings settings=new SessionSettings("Acceptor.cfg");
+			FixDealer server=new FixDealer();
 			FileStoreFactory storeFactory=new FileStoreFactory(settings);
 			ScreenLogFactory logFactory=new ScreenLogFactory(settings);
-			Acceptor acceptor = new SocketAcceptor
+			acceptor = new SocketAcceptor
 					      (server, storeFactory, settings, logFactory, new DefaultMessageFactory());
 			acceptor.start();
 		} catch(Exception e) {

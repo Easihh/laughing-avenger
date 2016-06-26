53dcd6e983af1c9797c67723ddf6e05462e06e94
Enrico Talbot <EnricoTalbot@gmail.com>
2016-06-26 10:56:42 -0400

    -Chart now use Number axis for X-axis but show value as String. -Early work for getting last 24 hours data point via RMI.

--- ThanatosClient/src/com/thanatos/QuoteChartController.java
+++ ThanatosClient/src/com/thanatos/QuoteChartController.java
@@ -1,45 +1,57 @@
 package com.thanatos;
 
 import java.net.URL;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import com.thanatos.shared.RmiQuote;
+import com.thanatos.shared.RmiQuoteIntf;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.ScrollPane;
 
 public class QuoteChartController implements Initializable{
     
 	@FXML
-	private LineChart<String, Double> qChart;
+	private LineChart<Double, Double> qChart;
 	@FXML
-	private ScrollPane chartScrollPane;
-	
+	private NumberAxis xQuoteChartAxis;
+    private Registry myReg;
+    private RmiQuoteIntf rmi;
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		//chartScrollPane.setPrefWidth(1000);
-		XYChart.Series<String,Double> series=new XYChart.Series<String,Double>();
-        //populating the series with data
-		series.getData().add(new XYChart.Data<String,Double>("1", 23.0));;
-        series.getData().add(new XYChart.Data<String,Double>("2", 14.0));
-        series.getData().add(new XYChart.Data<String,Double>("3", 15.0));
-        series.getData().add(new XYChart.Data<String,Double>("4", 24.0));
-        series.getData().add(new XYChart.Data<String,Double>("5", 34.0));
-        series.getData().add(new XYChart.Data<String,Double>("6", 36.0));
-        series.getData().add(new XYChart.Data<String,Double>("7", 22.0));
-		series.getData().add(new XYChart.Data<String,Double>("8", 23.0));;
-        series.getData().add(new XYChart.Data<String,Double>("9", 14.0));
-        series.getData().add(new XYChart.Data<String,Double>("10", 15.0));
-        series.getData().add(new XYChart.Data<String,Double>("11", 24.0));
-        series.getData().add(new XYChart.Data<String,Double>("12", 34.0));
-        series.getData().add(new XYChart.Data<String,Double>("13", 36.0));
-        series.getData().add(new XYChart.Data<String,Double>("14", 22.0));
-        
-        qChart.getData().add(series);
+		try {
+			myReg=LocateRegistry.getRegistry("127.0.0.1",5055);
+			rmi=(RmiQuoteIntf)myReg.lookup("quote");
+			List<RmiQuote> myquote=rmi.getLast24HoursQuoteInfo("GOOG");
+			qChart.getStyleClass().add("thick-chart");
+			xQuoteChartAxis.setTickLabelFormatter(new NumberAxis.DefaultFormatter(xQuoteChartAxis){
+				 @Override public String toString(Number object) { 
+					 String removeDecimal=Integer.toString(object.intValue());
+					 String amOrPm=(object.intValue()>=12 && object.intValue()<24) ? "PM":"AM";
+					 return String.format("%1$s:00"+amOrPm, removeDecimal); }
+			 });
+			XYChart.Series<Double,Double> series=new XYChart.Series<Double,Double>();
+			series.getData().add(new XYChart.Data<Double,Double>(10.0, 23.0));
+			series.getData().add(new XYChart.Data<Double,Double>(10.1, 21.0));
+	        series.getData().add(new XYChart.Data<Double,Double>(10.2, 14.0));
+	        series.getData().add(new XYChart.Data<Double,Double>(11.0, 15.0));
+	        series.getData().add(new XYChart.Data<Double,Double>(12.0, 24.0));
+	        series.getData().add(new XYChart.Data<Double,Double>(12.30, 34.0));
+	        series.getData().add(new XYChart.Data<Double,Double>(16.00, 22.0));
+	        qChart.getData().add(series);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void refreshMonitor() {
 		System.out.println("Refreshing Chart View");
+		
 	}
 }

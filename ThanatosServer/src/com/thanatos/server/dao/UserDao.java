b2916c202ad59c935bab1b4ab65b6ae03c30b695
Enrico Talbot <ortega_enrico@hotmail.com>
2021-08-21 14:06:38 -0700

    Delete ThanatosServer directory

--- ThanatosServer/src/com/thanatos/server/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.thanatos.server.dao;
-
-
-public interface UserDao {
-	
-	public boolean getUserByUsernamePassword(String username,String password);
-	
-}
--- ThanatosServer/src/com/thanatos/server/main/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.thanatos.server.main;
-
-public enum OrderStatus{
-	PENDING;
-}
